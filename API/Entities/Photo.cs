4d1089e566bf808431a3d5b4eadc03c3d4de7ca4
qthang02 <nguyenquocthang909@gmail.com>
2023-02-22 21:48:54 +0700

    config env dev and added AppUser and Photo Entities.

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Entities;
+
+public class Photo
+{
+    public int Id { get; set; }
+    public string? Url { get; set; }
+    public bool IsMain { get; set; }
+    public string? PublishId { set; get; }
+    public int AppUserId { get; set; }
+    public AppUser? AppUser { get; set; }
+}
