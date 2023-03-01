c0d5f68ae98e770c4828ad4dfa4a00ca6fdcdee9
qthang02 <nguyenquocthang909@gmail.com>
2023-03-01 10:24:15 +0700

    created User model and account.service.ts (include login, register, setCurrentUser and Logout)

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment.development";
+import {BehaviorSubject, map} from "rxjs";
+import {User} from "../_models/user";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  baseUrl = environment.apiUrl;
+  private currentUserSource= new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if(user) {
+          this.setCurrentUser(user)
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if(user) {
+          this.setCurrentUser(user)
+        }
+      })
+    );
+  }
+
+  setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+
+}
